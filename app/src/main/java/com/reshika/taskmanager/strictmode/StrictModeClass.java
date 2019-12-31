adfbd3d7c1b5e1d54ed559b8de01224f5f23dd67
Reshika <reshikabhasink@gmail.com>
2019-12-31 14:33:16 +0545

    signup and image browse

--- app/src/main/java/com/reshika/taskmanager/strictmode/StrictModeClass.java
+++ app/src/main/java/com/reshika/taskmanager/strictmode/StrictModeClass.java
@@ -1,4 +1,10 @@
 package com.reshika.taskmanager.strictmode;
 
 public class StrictModeClass {
+    public static void StrictMode(){
+        android.os.StrictMode.ThreadPolicy policy= new android.os.StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+
+        android.os.StrictMode.setThreadPolicy(policy);
+    }
 }
